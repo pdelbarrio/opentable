ed49d33b967d4495e4bd467477d1069e575e6a8c
Pablo del Barrio <pablodbb@gmail.com>
2023-03-02 17:18:00 +0100

    Homepage structure and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>I want to learn Next 13</h1>
-    </main>
-  );
+  return <main></main>;
 }
